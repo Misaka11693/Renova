ddcf79994c511512106dc1d54a07a2a3aa66967b
Misaka11639 <1647988592@qq.com>
2025-07-22 11:36:27 +0800

    添加项目文件。

---
+++ src/Renova.WebApi/ApiSetup.cs
@@ -0,0 +1,22 @@
+﻿namespace Renova;
+
+public static class ApiSetup
+{
+    /// <summary>
+    /// 配置服务
+    /// </summary>
+    /// <param name="builder"></param>
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    /// <summary>
+    /// 配置 HTTP 请求管道
+    /// </summary>
+    /// <param name="app"></param>
+    public static WebApplication UseMiddlewares(this WebApplication app)
+    {
+        return app;
+    }
+}
