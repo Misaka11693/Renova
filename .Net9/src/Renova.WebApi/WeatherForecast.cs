382dd68bff62bb73e5c931a7d3ca7c33415b336e
Misaka11639 <1647988592@qq.com>
2025-07-24 17:35:36 +0800

    天气预报模型添加注释信息

--- .Net9/src/Renova.WebApi/WeatherForecast.cs
+++ .Net9/src/Renova.WebApi/WeatherForecast.cs
@@ -1,13 +1,28 @@
 namespace Renova
 {
+    /// <summary>
+    /// 天气预报模型
+    /// </summary>
     public class WeatherForecast
     {
+        /// <summary>
+        /// 日期
+        /// </summary>
         public DateOnly Date { get; set; }
 
+        /// <summary>
+        /// 摄氏度温度
+        /// </summary>
         public int TemperatureC { get; set; }
 
+        /// <summary>
+        /// 华氏温度
+        /// </summary>
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        /// <summary>
+        /// 天气摘要
+        /// </summary>
         public string? Summary { get; set; }
     }
 }
