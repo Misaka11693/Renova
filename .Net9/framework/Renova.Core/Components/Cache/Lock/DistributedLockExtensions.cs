0d940f1098d0c8028ec6e5e6aa44f15a8dec7a31
Misaka11639 <1647988592@qq.com>
2025-10-27 21:36:11 +0800

    分布式锁扩展方法方法调整

--- .Net9/framework/Renova.Core/Components/Cache/Lock/DistributedLockExtensions.cs
+++ .Net9/framework/Renova.Core/Components/Cache/Lock/DistributedLockExtensions.cs
@@ -15,7 +15,7 @@ public static class DistributedLockExtensions
     /// <param name="timeoutSeconds">锁超时时间（秒，默认30）</param>
     /// <param name="autoDelay">是否自动续期（默认true）</param>
     /// <returns>锁控制器或 null（超时未获取到锁）</returns>
-    public static async Task<LockController?> LockAsync(this ICache cache, string name, int timeoutSeconds = 30, bool autoDelay = true)
+    public static LockController? Lock(this ICache cache, string name, int timeoutSeconds = 30, bool autoDelay = true)
     {
         if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
 
@@ -32,7 +32,8 @@ public static class DistributedLockExtensions
                 return new LockController(cache, name, value, timeoutSeconds, refreshInterval, autoDelay, CancellationToken.None);
             }
 
-            await Task.Delay(3);//延迟3毫秒重试
+            //await Task.Delay(3);//延迟3毫秒重试
+            Thread.Sleep(3);
         }
 
         return null;
@@ -47,7 +48,7 @@ public static class DistributedLockExtensions
     /// <param name="timeoutSeconds">锁超时时间（秒，默认30）</param>
     /// <param name="autoDelay">是否自动续期（默认true）</param>
     /// <returns>锁控制器或 null（获取失败）</returns>
-    public static LockController? TryLockAsync(this ICache cache, string name, int timeoutSeconds = 30, bool autoDelay = true)
+    public static LockController? TryLock(this ICache cache, string name, int timeoutSeconds = 30, bool autoDelay = true)
     {
         if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
 
