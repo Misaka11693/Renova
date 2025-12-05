195cccde123c4d3d215d80aafff639a2e57035ad
Misaka11639 <1647988592@qq.com>
2025-12-05 09:52:10 +0800

    添加用户友好异常类

---
+++ .Net9/framework/Renova.Core/Components/Exceptions/UserFriendlyException.cs
@@ -0,0 +1,31 @@
+﻿namespace System;
+
+/// <summary>
+/// 用户友好异常
+/// </summary>
+public class UserFriendlyException : Exception
+{
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    public UserFriendlyException()
+    {
+    }
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="message">异常消息</param>
+    public UserFriendlyException(string message)
+        : base(message)
+    {
+    }
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="message">异常消息</param>
+    /// <param name="innerException">内部异常</param>
+    public UserFriendlyException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
